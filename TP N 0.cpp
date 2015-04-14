250a089d87b7d3ccd444c5b78cf970c7bfc25946
Erik Rodriguez <erikemanuelrodriguez@gmail.com>
2015-04-14 16:13:06 -0300

    Hola Mundo.cpp

---
+++ TP N 0.cpp
@@ -0,0 +1,13 @@
+//Descripcion: Hola Mundo
+//Alumno: Erik Emanuel Rodriguez Mereles
+//Fecha:  14/04/2015
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf ("Hola Mundo \n \n");
+	system("PAUSE");
+	return 0;
+}
